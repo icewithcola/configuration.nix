fd2ba08655bc31ff58006af8547d4f613f87d490
icewithcola <me@kagurach.uk>
2024-09-19 13:55:00 +0800

    (hardware):Add OpenGL specification

--- configuration.nix
+++ configuration.nix
@@ -39,7 +39,6 @@
   networking = {
     hostName = "kagura-notebook";
     networkmanager.enable = true;
-    nftables.enable = true;
     firewall.enable = false;
   };
 
@@ -153,8 +152,14 @@
     nix-ld.enable = true;
   };
 
-
   zramSwap.enable = true;
+
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+    };
+  };
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
