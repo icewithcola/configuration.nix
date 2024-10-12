17c6656f7f0d3faa3f66dbbd8eebb8b21a174f01
icewithcola <me@kagurach.uk>
2024-10-12 10:02:16 +0800

    add virt-managerl; bump version

--- configuration.nix
+++ configuration.nix
@@ -168,6 +168,9 @@
     };
   };
 
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
