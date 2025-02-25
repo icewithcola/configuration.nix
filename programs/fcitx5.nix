36b9c0904a02dfda0d7f295699d7a521ec885c91
icewithcola <me@kagurach.uk>
2025-02-25 19:34:19 +0800

    fcitx use stable

--- programs/fcitx5.nix
+++ programs/fcitx5.nix
@@ -1,10 +1,10 @@
-{ pkgs-unstable, ... }:
+{ pkgs, ... }:
 {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5.waylandFrontend = true;
-    fcitx5.addons = with pkgs-unstable; [
+    fcitx5.addons = with pkgs; [
       fcitx5-gtk             # alternatively, kdePackages.fcitx5-qt
       fcitx5-chinese-addons  # table input method support
       fcitx5-nord            # a color theme
