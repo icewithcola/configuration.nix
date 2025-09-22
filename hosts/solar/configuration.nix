3b298ddb8ff9d7e14760058fa25dadca68623bd2
icewithcola <me@kagurach.uk>
2025-09-22 22:48:35 +0800

    solar: init

---
+++ hosts/solar/configuration.nix
@@ -0,0 +1,176 @@
+{
+  config,
+  lib,
+  pkgs,
+  pkgs-stable,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages;
+    kernelParams = [ "nomodeset" ];
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        efiSysMountPoint = "/boot";
+        canTouchEfiVariables = true;
+      };
+    };
+    sysctl = {
+      "net.ipv4.conf.all.forwarding" = true;
+      "net.ipv6.conf.all.forwarding" = true;
+
+      "net.ipv6.conf.all.accept_ra" = 0;
+      "net.ipv6.conf.all.autoconf" = 0;
+      "net.ipv6.conf.all.use_tempaddr" = 0;
+
+      "net.ipv6.conf.${name}.accept_ra" = 2;
+      "net.ipv6.conf.${name}.autoconf" = 1;
+    };
+  };
+
+  # https://github.com/ghostbuster91/blogposts/blob/a2374f0039f8cdf4faddeaaa0347661ffc2ec7cf/router2023-part2/main.md
+  systemd.network = {
+    wait-online.anyInterface = true;
+    netdevs = {
+      "20-br-lan" = {
+        netdevConfig = {
+          Kind = "bridge";
+          Name = "br-lan";
+        };
+      };
+    };
+
+    networks = {
+      "10-wan" = {
+        matchConfig.Name = "enp1s0";
+        networkConfig = {
+          DHCP = "ipv4";
+          DNSOverTLS = true;
+          DNSSEC = true;
+          IPv6PrivacyExtensions = false;
+          IPForward = true;
+        };
+        linkConfig.RequiredForOnline = "routable";
+      };
+      "30-lan" = {
+        matchConfig.Name = "enp2s0";
+        linkConfig.RequiredForOnline = "enslaved";
+        networkConfig = {
+          ConfigureWithoutCarrier = true;
+        };
+      };
+      "40-br-lan" = {
+        matchConfig.Name = "br-lan";
+        bridgeConfig = { };
+        address = [
+          "192.168.114.1/24"
+        ];
+        networkConfig = {
+          ConfigureWithoutCarrier = true;
+        };
+      };
+    };
+  };
+  networking = {
+    hostName = "solar";
+    useDHCP = false;
+
+    vlans = {
+      wan = {
+        id = 10;
+        interface = "enp1s0";
+      };
+      lan = {
+        id = 20;
+        interface = "enp2s0";
+      };
+    };
+
+    interfaces = {
+      wan.useDHCP = true;
+      lan = {
+        ipv4.addresses = [
+          {
+            address = "192.168.114.1";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+
+    nftables = {
+      enable = true;
+      ruleset = ''
+        table inet filter {
+          chain input {
+            type filter hook input priority 0; policy drop;
+
+            iifname { "br-lan" } accept comment "Allow local network to access the router"
+            iifname "wan" ct state { established, related } accept comment "Allow established traffic"
+            iifname "wan" icmp type { echo-request, destination-unreachable, time-exceeded } counter accept comment "Allow select ICMP"
+            iifname "wan" counter drop comment "Drop all other unsolicited traffic from wan"
+            iifname "lo" accept comment "Accept everything from loopback interface"
+          }
+          chain forward {
+            type filter hook forward priority filter; policy drop;
+
+            iifname { "br-lan" } oifname { "wan" } accept comment "Allow trusted LAN to WAN"
+            iifname { "wan" } oifname { "br-lan" } ct state { established, related } accept comment "Allow established back to LANs"
+          }
+        }
+
+        table ip nat {
+          chain postrouting {
+            type nat hook postrouting priority 100; policy accept;
+            oifname "wan" masquerade
+          }
+        }
+      '';
+    };
+
+    nat.enable = false;
+    firewall.enable = false;
+  };
+
+  time.timeZone = "Asia/Shanghai";
+
+  users.users.kagura = {
+    isNormalUser = true;
+    home = "/home/kagura";
+    extraGroups = [
+      "wheel"
+      "kvm"
+      "incus-admin"
+      "docker"
+      "libvirtd"
+    ];
+    shell = pkgs.zsh;
+  };
+
+  environment.systemPackages =
+    (with pkgs; [
+    ])
+    ++ (with pkgs-stable; [
+    ]);
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Desktop Environment
+  services = {
+    xserver.enable = false;
+  };
+
+  programs = {
+    zsh.enable = true;
+  };
+
+  zramSwap.enable = false;
+
+  system.stateVersion = "24.05";
+}
---
+++ hosts/solar/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  inputs,
+  config,
+  ...
+}:
+let
+  enableNixOSModule = (x: lib.map (s: ../../nixosModules/${s}) x);
+  enablePrograms = (x: lib.map (s: ../../programs/${s}.nix) x);
+  enableServices = (x: lib.map (s: ../../services/${s}.nix) x);
+in
+{
+  imports = ([
+    ./configuration.nix
+  ])
+  ++ (enableNixOSModule [
+  ])
+  ++ (enablePrograms [
+    "incus"
+  ])
+  ++ (enableServices [
+    "sshd"
+    "ddns"
+  ]);
+
+  kagura.ddns = {
+    enable = false;
+    host = "rin";
+    secretFile = config.age.secrets.ddns-token.path;
+    interface = "enp7s0f0";
+  };
+}
