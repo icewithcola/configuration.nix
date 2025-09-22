baec755af9ef9efa299889612adba33480437d93
icewithcola <me@kagurach.uk>
2025-09-22 23:06:24 +0800

    solar: update kernel ra

--- hosts/solar/configuration.nix
+++ hosts/solar/configuration.nix
@@ -28,9 +28,6 @@
       "net.ipv6.conf.all.accept_ra" = 0;
       "net.ipv6.conf.all.autoconf" = 0;
       "net.ipv6.conf.all.use_tempaddr" = 0;
-
-      "net.ipv6.conf.${name}.accept_ra" = 2;
-      "net.ipv6.conf.${name}.autoconf" = 1;
     };
   };
 
