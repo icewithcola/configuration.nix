e27c6d18ae5c2238f9ab00e37247e1785cff3d7a
icewithcola <me@kagurach.uk>
2025-09-22 23:07:11 +0800

    rin: fix sysctl

--- hosts/solar/configuration.nix
+++ hosts/solar/configuration.nix
@@ -21,7 +21,7 @@
         canTouchEfiVariables = true;
       };
     };
-    sysctl = {
+    kernel.sysctl = {
       "net.ipv4.conf.all.forwarding" = true;
       "net.ipv6.conf.all.forwarding" = true;
 
