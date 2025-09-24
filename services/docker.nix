af10238c3e56f6c163402303ea43136873432434
icewithcola <me@kagurach.uk>
2025-09-24 11:52:19 +0800

    docker: make btrfs only used in btrfs

--- services/docker.nix
+++ services/docker.nix
@@ -1,12 +1,19 @@
-{ pkgs, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 {
   virtualisation.docker = {
     enable = true;
     package = pkgs.docker;
-    storageDriver = config.kagura.rootFileSystem;
     rootless = {
       enable = true;
       setSocketVariable = true;
     };
+  }
+  // lib.mkIf (config.kagura.rootFileSystem == "btrfs") {
+    storageDriver = "btrfs"; # else use overlayfs
   };
 }
