edc4ec1610f40d6433aca1a21e9659d31482f106
icewithcola <me@kagurach.uk>
2025-09-24 11:11:30 +0800

    ssh: update ips

--- home/terminal/ssh.nix
+++ home/terminal/ssh.nix
@@ -4,6 +4,7 @@
     enable = true;
     enableDefaultConfig = false;
     matchBlocks = {
+      # Git services
       "github.com" = {
         host = "github.com";
         hostname = "ssh.github.com";
@@ -16,24 +17,36 @@
         port = 22;
         user = "root";
       };
-      "remote.dorm.lan" = {
-        host = "remote.dorm.lan";
+
+      # Remote controls
+      "remote.nas" = {
+        host = "remote.nas";
         hostname = "ssh.git.kagurach.uk";
         port = 30072;
         user = "root";
       };
-      "router.dorm.lan" = {
-        host = "router.dorm.lan";
-        hostname = "192.168.10.1";
-        port = 22;
-        user = "11451";
-      };
-      "nas.dorm.lan" = {
-        host = "nas.dorm.lan";
-        hostname = "192.168.10.150";
+
+      # Nekohouse Local servers
+      "j1900.nekohouse" = {
+        host = "j1900.nekohouse";
+        hostname = "192.168.114.1";
         port = 22;
         user = "root";
       };
+      "router.nekohouse" = {
+        host = "router.nekohouse";
+        hostname = "192.168.10.1";
+        port = 22;
+        user = "root";
+      };
+      "nas.nekohouse" = {
+        host = "nas.nekohouse";
+        hostname = "192.168.10.149";
+        port = 22;
+        user = "root";
+      };
+
+      # Nekohouse IPv6 servers
       "home.kagura.lolicon.cyou" = {
         host = "home.kagura.lolicon.cyou";
         hostname = "home.kagura.lolicon.cyou";
