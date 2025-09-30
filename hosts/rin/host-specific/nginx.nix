3cacf3b799aa2c0166df6e50145567fd6b8e6f3e
icewithcola <me@kagurach.uk>
2025-09-30 21:30:08 +0800

    rin/nginx: fix cert

--- hosts/rin/host-specific/nginx.nix
+++ hosts/rin/host-specific/nginx.nix
@@ -12,12 +12,13 @@ in
     virtualHosts = {
       "rin.home.${baseName}" = {
         onlySSL = true;
+        sslCertificate = config.age.secrets.loli-cer.path;
+        sslCertificateKey = config.age.secrets.loli-priv.path;
+
         locations = {
           "/" = {
             proxyPass = "http://127.0.0.1:2283";
             proxyWebsockets = true;
-            sslCertificate = config.age.secrets.loli-cer.path;
-            sslCertificateKey = config.age.secrets.loli-priv.path;
           };
         };
       };
