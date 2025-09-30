0a42a2aa6dc9550cf1eeb052fc213843e45ced9e
icewithcola <me@kagurach.uk>
2025-09-30 22:02:04 +0800

    rin/nginx: update proxy

--- hosts/rin/host-specific/nginx.nix
+++ hosts/rin/host-specific/nginx.nix
@@ -17,8 +17,14 @@ in
 
         locations = {
           "/" = {
-            proxyPass = "http://127.0.0.1:2283";
+            proxyPass = "http://127.0.0.1:2283/";
             proxyWebsockets = true;
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+            '';
           };
         };
       };
