5271bf0f69a861ec8c1d370d649a2027a3fd9ce1
icewithcola <me@kagurach.uk>
2025-09-30 21:43:36 +0800

    age: maybe i am always wrong

--- secrets/default.nix
+++ secrets/default.nix
@@ -4,18 +4,32 @@ let
   files = filter (x: hasSuffix ".age" x) (builtins.attrNames (builtins.readDir ./.));
 in
 {
-  age.secrets = listToAttrs (
-    map (
-      name:
-      let
-        fileName = removeSuffix ".age" name;
-      in
+  age.secrets =
+    lib.recursiveUpdate
+      (listToAttrs (
+        map (
+          name:
+          let
+            fileName = removeSuffix ".age" name;
+          in
+          {
+            name = fileName;
+            value.file = ./${name};
+          }
+        ) files
+      ))
       {
-        name = fileName;
-        value.file = ./${name};
-      }
-    ) files
-  );
+        "loli-cer" = {
+          mode = "770";
+          owner = "nginx";
+          group = "nginx";
+        };
+        "loli-priv" = {
+          mode = "770";
+          owner = "nginx";
+          group = "nginx";
+        };
+      };
 
   age.identityPaths = [
     "/home/kagura/.ssh/id_ed25519"
