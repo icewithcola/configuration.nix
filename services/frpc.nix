4da315a2c6f2c9d3e1f39b603d7cbc7cf09640a8
icewithcola <me@kagurach.uk>
2025-11-06 19:40:42 +0800

    frp: use config

--- services/frpc.nix
+++ services/frpc.nix
@@ -1,33 +1,60 @@
 { lib, config, ... }:
+let
+  inherit (lib) mkOption types;
+in
 {
-  services.frp = {
-    enable = true;
-    role = "client";
-    settings = {
-      serverAddr = "ssh.git.kagurach.uk";
-      serverPort = 30071;
+  options.kagura.frp = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable frp, client";
+    };
 
-      auth.tokenSource = {
-        type = "file";
-        file = config.age.secrets.frp-token.file;
-      };
+    remoteServer = mkOption {
+      type = types.str;
+      example = "server.your.domain";
+      description = "The remote server, domain or ip";
+    };
 
-      proxies = [
-        {
-          name = "web";
-          type = "http";
-          localIP = "127.0.0.1";
-          localPort = 45323;
-          customDomains = [ "al.lolicon.cyou" ];
-        }
-        {
-          name = "ssh";
-          type = "tcp";
-          localIP = "127.0.0.1";
-          localPort = 22;
-          remotePort = 30072;
-        }
-      ];
+    port = mkOption {
+      type = types.int;
+      example = 11451;
+      description = "The port of the remote server";
     };
   };
+
+  services.frp =
+    let
+      cfg = config.kagura.frp;
+    in
+    lib.mkIf cfg.enable {
+      enable = true;
+      role = "client";
+      settings = {
+        serverAddr = cfg.remoteServer;
+        serverPort = cfg.port;
+
+        auth.tokenSource = {
+          type = "file";
+          file = config.age.secrets.frp-token.file;
+        };
+
+        proxies = [
+          {
+            name = "web";
+            type = "http";
+            localIP = "127.0.0.1";
+            localPort = 45323;
+            customDomains = [ "al.lolicon.cyou" ];
+          }
+          {
+            name = "ssh";
+            type = "tcp";
+            localIP = "127.0.0.1";
+            localPort = 22;
+            remotePort = 30072;
+          }
+        ];
+      };
+    };
 }
