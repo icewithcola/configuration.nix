0d30e184b4bed7ac501d64456bda4340c1da7c95
icewithcola <me@kagurach.uk>
2025-12-26 23:34:15 +0800

    emilia: mount data part

--- hosts/emilia/hardware-configuration.nix
+++ hosts/emilia/hardware-configuration.nix
@@ -1,30 +1,46 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "ahci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/0905329d-4022-4692-bb5d-b614c117dd8c";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/0905329d-4022-4692-bb5d-b614c117dd8c";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2bab0d29-6c12-4665-83d4-f986119f946f";
-      fsType = "ext4";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/2bab0d29-6c12-4665-83d4-f986119f946f";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/2A8A-6A4E";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-uuid/2A8A-6A4E";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  fileSystems."/mnt/data" = {
+    device = "/dev/disk/by-uuid/a6f35d56-b70d-4474-a2d8-762d6980c352 ";
+    fsType = "ext4";
+    noCheck = true;
+    options = [ "nofail" ];
+  };
 
   swapDevices = [ ];
 
