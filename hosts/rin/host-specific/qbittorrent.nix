aea236e2748e349eb8a8ed439bbba75f01cfb25c
icewithcola <me@kagurach.uk>
2026-01-21 13:31:54 +0800

    rin: torrent generate password

--- hosts/rin/host-specific/qbittorrent.nix
+++ hosts/rin/host-specific/qbittorrent.nix
@@ -2,11 +2,20 @@
   pkgs,
   config,
   ...
-}: {
+}:
+{
   config.services.qbittorrent = {
     enable = true;
     package = pkgs.qbittorrent-enhanced-nox;
     webuiPort = 41654;
-
+    serverConfig = {
+      LegalNotice.Accepted = true;
+      Preferences = {
+        WebUI = {
+          Username = "kagura";
+          Password_PBKDF2 = "sSkoxAleUAPfx19a2sH7CA==:+DsnF+AxFuWdfaX+T2nBYFGbmnibM4LbG4tPKaiZy2+tLcJPsf09Rf0m9ykDfrbpcl5X7W4VyFq8+02rpa/YTw==";
+        };
+      };
+    };
   };
 }
