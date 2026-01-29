16c9b4cdd9fe8d25624caf2ba6799fb03e4ae976
icewithcola <me@kagurach.uk>
2026-01-29 22:12:29 +0800

    rin: route to our cm network

--- hosts/rin/host-specific/tailscale.nix
+++ hosts/rin/host-specific/tailscale.nix
@@ -5,6 +5,8 @@
     useRoutingFeatures = "both";
     extraUpFlags = [
       "--advertise-routes=192.168.114.0/24"
+      "--advertise-routes=192.168.23.0/24"
+      "--advertise-routes=fe80::/8"
       "--advertise-exit-node"
     ];
 
