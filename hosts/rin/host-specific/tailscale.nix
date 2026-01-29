2d86a479d3c9c4e8456063ddbafe1f1e19576043
icewithcola <me@kagurach.uk>
2026-01-30 00:13:00 +0800

    fix: tailscale up command

--- hosts/rin/host-specific/tailscale.nix
+++ hosts/rin/host-specific/tailscale.nix
@@ -4,9 +4,7 @@
     enable = true;
     useRoutingFeatures = "both";
     extraUpFlags = [
-      "--advertise-routes=192.168.114.0/24"
-      "--advertise-routes=192.168.23.0/24"
-      "--advertise-routes=fe80::/8"
+      "--advertise-routes=192.168.114.0/24,192.168.23.0/24,fe80::/8"
       "--advertise-exit-node"
     ];
 
