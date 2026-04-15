25780e5c355c27731bb075203d285f7b4839ed7b
icewithcola <me@kagurach.uk>
2026-04-15 14:34:04 +0800

    rin: pin driver

--- hosts/rin/host-specific/p106.nix
+++ hosts/rin/host-specific/p106.nix
@@ -10,6 +10,7 @@
       nvidiaSettings = false;
       nvidiaPersistenced = true;
       open = true;
+      package = config.boot.kernelPackages.nvidiaPackages.legacy_580; # https://github.com/NixOS/nixpkgs/commit/893218f404d979b7cb87afab8aa656f5a5d7ca91
     };
   };
 }
