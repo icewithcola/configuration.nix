2613bc7765ee4d03f18d5c8b2d1fb47ad9e8fa38
icewithcola <me@kagurach.uk>
2026-08-14 16:01:47 +0800

    agenix: scope secrets by host

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,41 +1,60 @@
 {
   lib,
   config,
+  host,
   ...
 }:
 let
-  inherit (lib) filter hasSuffix listToAttrs removeSuffix;
-  files = filter (x: hasSuffix ".age" x) (builtins.attrNames (builtins.readDir ./.));
+  secretFiles = {
+    github-token = ./github-token.age;
+    ddns-token = ./ddns-token.age;
+    tailscale-emilia = ./tailscale-emilia.age;
+    tailscale-kagura-notebook = ./tailscale-kagura-notebook.age;
+    tailscale-rin = ./tailscale-rin.age;
+    wireguard-dn42 = ./wireguard-dn42.age;
+  };
+
+  hostSecrets = {
+    emilia = [
+      "github-token"
+      "tailscale-emilia"
+    ];
+    kagura-notebook = [
+      "github-token"
+      "tailscale-kagura-notebook"
+    ];
+    rin = [
+      "github-token"
+      "ddns-token"
+      "tailscale-rin"
+      "wireguard-dn42"
+    ];
+  };
+
+  secretsForHost = hostSecrets.${host} or [ "github-token" ];
 in
 {
   age.secrets =
-    lib.recursiveUpdate
-      (listToAttrs (
-        map (
-          name:
-          let
-            fileName = removeSuffix ".age" name;
-          in
-          {
-            name = fileName;
-            value.file = ./${name};
-          }
-        ) files
-      ))
-      {
-        "loli-cer" = lib.mkIf config.services.nginx.enable {
-          mode = "770";
-          owner = "nginx";
-          group = "nginx";
-          file = ./loli-cer.age;
-        };
-        "loli-priv" = lib.mkIf config.services.nginx.enable {
-          mode = "770";
-          owner = "nginx";
-          group = "nginx";
-          file = ./loli-priv.age;
-        };
+    lib.listToAttrs (
+      map (name: {
+        inherit name;
+        value.file = secretFiles.${name};
+      }) secretsForHost
+    )
+    // lib.optionalAttrs config.services.nginx.enable {
+      "loli-cer" = {
+        mode = "770";
+        owner = "nginx";
+        group = "nginx";
+        file = ./loli-cer.age;
       };
+      "loli-priv" = {
+        mode = "770";
+        owner = "nginx";
+        group = "nginx";
+        file = ./loli-priv.age;
+      };
+    };
 
   age.identityPaths = [
     "/home/kagura/.ssh/id_ed25519"
